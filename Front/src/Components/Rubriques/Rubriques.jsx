fbc2c5676d69fc65f814bb6e08b4f16685f9cd92
Roy-ElHaddad <royhaddad27@gmail.com>
2023-08-11 15:07:02 +0200

    added remove functionality

--- Front/src/Components/Rubriques/Rubriques.jsx
+++ Front/src/Components/Rubriques/Rubriques.jsx
@@ -1,29 +1,41 @@
-// import "./Rubriques.css"
+// import React from "react";
+// import "./Rubriques.css";
 
-
-// export default function Rubriques( rub ,setRub) {
-//    console.log(rub)
+// export default function Rubriques({ rub , setRub}) {
 //     return (
 //         <div className="main-container">
-//                 <li className="rubriques-container" >
-//                     <h1> {rub.key} : </h1>
+//             {rub.map((r) => (
+//                 <li className="rubriques-container" key={r.id}>
+//                     <h1> {r.key} :</h1>
 //                     <div className="rubriques-input">
-//                         <input type="text" value={rub.title} />
+//                         <input type="text" value={r.title} readOnly />
 //                     </div>
 //                     <div className="rubriques-submit">
-//                         <button className="modify"><span className="material-symbols-outlined"  >move_group</span></button>
-//                         <button><span className="material-symbols-outlined">remove_circle</span></button>
+//                         <button className="modify">
+//                             <span className="material-symbols-outlined">
+//                                 move_group
+//                             </span>
+//                         </button>
+//                         <button>
+//                             <span className="material-symbols-outlined">
+//                                 remove_circle
+//                             </span>
+//                         </button>
 //                     </div>
 //                 </li>
-//             {/* ))} */}
-//         </div> 
-//     )
+//             ))}
+//         </div>
+//     );
 // }
-
 import React from "react";
 import "./Rubriques.css";
 
-export default function Rubriques({ rub , setRub}) {
+export default function Rubriques({ rub, setRub }) {
+    const handleRemoveRub = (id) => {
+        const updatedRub = rub.filter((r) => r.id !== id);
+        setRub(updatedRub);
+    };
+
     return (
         <div className="main-container">
             {rub.map((r) => (
@@ -38,7 +50,9 @@ export default function Rubriques({ rub , setRub}) {
                                 move_group
                             </span>
                         </button>
-                        <button>
+                        <button
+                            onClick={() => handleRemoveRub(r.id)} // Call the handler with the rub's ID
+                        >
                             <span className="material-symbols-outlined">
                                 remove_circle
                             </span>
